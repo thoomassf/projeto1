a96562eafdaadd09466979673cc479b2c7afe28c
Thomas Farias <thomas.farias@sempreit.com.br>
2024-08-22 17:15:03 -0300

    create recipes routes

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+# from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
